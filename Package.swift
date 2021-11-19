b726f5a07b638bc9b035ccc2202248090fcde12d
ITzTravelIntime <23339020+ITzTravelInTime@users.noreply.github.com>
2021-11-19 16:07:41 +0100

    Switched package dependancies to releases, in preparation to a public release.

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/ITzTravelInTime/TINURecovery", .branch("main")),
-        .package(url: "https://github.com/ITzTravelInTime/SwiftLoggedPrint", .branch("main"))
+        .package(url: "https://github.com/ITzTravelInTime/TINURecovery", from: "4.0.1"),
+        .package(url: "https://github.com/ITzTravelInTime/SwiftLoggedPrint", from: "3.2.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
